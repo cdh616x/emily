f786ad3714380bba90a0226f7a997aee12cc8cf6
cdh616x <91150236+cdh616x@users.noreply.github.com>
2022-01-02 22:59:17 -0500

    heroku debugging

---
+++ config/keys.js
@@ -0,0 +1,8 @@
+//jshint esversion:6
+
+module.exports = {
+  googleClientID: "122399910087-hnaei8n1uobekdci2nijtua28q00hdmk.apps.googleusercontent.com",// ----- shared with the public: identifies app to google servers
+  googleClientSecret: "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"//----- do not share with anyone
+};
+//can now require both properties into another file i.e. index.js
+//this pattern makes chunks of code available to other files within the application
